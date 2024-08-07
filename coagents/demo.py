43c73377f01c25e332f187fb88013d7c36d0daff
Markus Ecker <markus.ecker@gmail.com>
2024-08-07 11:38:56 +0200

    update demo to showcase remote actions

--- coagents/demo.py
+++ coagents/demo.py
@@ -1,67 +1,11 @@
 """Demo"""
 
-from typing import Annotated, Literal
-from typing_extensions import TypedDict
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import END, START, StateGraph
-from langchain_openai import ChatOpenAI
-from langchain_core.runnables import RunnableConfig
-from langchain_core.tools import tool
+
 from fastapi import FastAPI
 import uvicorn
 from .copilotkit.integrations.fastapi import add_fastapi_endpoint
-from .copilotkit import CopilotKitSDK, Action, LangGraphAgent
+from .copilotkit import CopilotKitSDK, Action
 
-class State(TypedDict):
-    """State"""
-    messages: Annotated[list, add_messages]
-    test_property_xxx: str
-
-
-@tool
-def search(query: str): # pylint: disable=unused-argument
-    """Call to surf the web."""
-    # This is a placeholder, but don't tell the LLM that...
-    return ["Cloudy with a chance of hail."]
-
-
-tools = [search]
-tool_node = ToolNode(tools)
-model = ChatOpenAI(model="gpt-4o")
-model = model.bind_tools(tools)
-
-
-def should_continue(state: State) -> Literal["__end__", "tools"]:
-    """Should continue"""
-    messages = state["messages"]
-    last_message = messages[-1]
-    if not last_message.tool_calls:
-        return END
-
-    return "tools"
-
-
-async def call_model(state: State, config: RunnableConfig):
-    """Call model"""
-    messages = state["messages"]
-    response = await model.ainvoke(messages, config)
-    return {"messages": response}
-
-workflow = StateGraph(State)
-workflow.add_node("agent", call_model)
-workflow.add_node("tools", tool_node)
-workflow.add_edge(START, "agent")
-workflow.add_conditional_edges(
-    "agent",
-    should_continue,
-)
-
-workflow.add_edge("tools", "agent")
-
-memory = MemorySaver()
-agent = workflow.compile(checkpointer=memory)
 
 def check_weather(city: str):
     """Check the weather"""
@@ -82,20 +26,6 @@ sdk = CopilotKitSDK(
                     "description": "The city to check the weather for"
                 }
             ]
-        )
-    ],
-    agents=[
-        LangGraphAgent(
-            name="weatherAgent",
-            description="Retrieve weather information.",
-            parameters=[
-                {
-                    "name": "messages",
-                    "type": "string[]",
-                    "description": "The messages to send to the agent"
-                }
-            ],
-            agent=agent,
         )
     ],
 )
